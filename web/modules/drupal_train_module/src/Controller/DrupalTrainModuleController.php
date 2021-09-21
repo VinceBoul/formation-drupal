6e301b20d6b0a0f09d05ece24cfa219a93e4315c
Vincent <toullec.0104@apside-groupe.com>
2021-09-21 16:20:21 +0200

    Gulp + SASS + form steps + [THEME] no container for extensions routes

--- web/modules/drupal_train_module/src/Controller/DrupalTrainModuleController.php
+++ web/modules/drupal_train_module/src/Controller/DrupalTrainModuleController.php
@@ -3,6 +3,7 @@
 namespace Drupal\drupal_train_module\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
 
 /**
  * Returns responses for Drupal Train Module routes.
@@ -14,9 +15,37 @@ class DrupalTrainModuleController extends ControllerBase {
    */
   public function build() {
 
+    $query = \Drupal::entityQuery('node')->condition('type', 'bol_etape');
+    $nids = $query->execute();
+    $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+
+    /**
+     * @var Node[] $stepsNodes
+     */
+    $stepsNodes = $node_storage->loadMultiple($nids);
+    $stepsNodeBis = $stepsNodes;
+
+    $step = array_pop($stepsNodeBis);
+    $answersId = $step->get('field_questions')->getValue();
+
+    $answers = [];
+    foreach ($answersId as $answer){
+      $answers[] = Node::load($answer['target_id']);
+    }
+
     return [
       '#theme' => 'my_template',
-      '#test_var' => $this->t('Test Value'),
+      '#data' => [
+          'test_value' => $this->t('Test Value'),
+          'form_steps' => $stepsNodes,
+          'currentStep' => $step,
+          'step_answers' => $answers
+      ],
+      '#attached' => [
+        'library' => [
+          'drupal_train_module/drupal_train_module',
+        ],
+      ],
     ];
 
   }
